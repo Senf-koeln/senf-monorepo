c7368000bfb183caba8cb7e181cfc1fdba5a2e01
Karl Keller <kgk86@hotmail.com>
2021-08-23 18:19:44 +0200

    Refactor login test

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -3,7 +3,7 @@
 const authUser = require("../fixtures/auth-user.json");
 
 describe("The Login Page", () => {
-  const { email, password } = authUser;
+  const { email, password, handle } = authUser;
   before(() => {
     cy.setCookie("Cookie_settings", "all");
     cy.visit("/");
@@ -13,22 +13,18 @@ describe("The Login Page", () => {
     cy.get(".FilterComponent")
       .find("[data-cy=open-signnote]")
       .click({ force: true });
-    // click on login / anmelden
     cy.get("[data-cy=login]").click();
-    // fill email and password
     cy.get("[data-cy=login-email]")
       .click()
-      .type(authUser.email)
+      .type(email)
     cy.get("[data-cy=login-password]")
       .click()
-      .type(authUser.password)
-    // submit button
+      .type(password)
     cy.get("[data-cy=login-user]").click();
-    // click the Profile button in Sidebar
     cy.get(".FilterComponent")
       .find("[data-cy=profile-button]")
       .click({ force: true });
     cy.get("[data-cy=hey-user]")
-      .should('contain.text', 'Hey ' + authUser.handle)
+      .should('contain.text', 'Hey ' + handle)
   });
 });
