33ad2d87d969d54ff15d107f6b7fc717d4135fa7
Karl Keller <kgk86@hotmail.com>
2021-08-23 18:15:39 +0200

    Remove unnecessary comments

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -6,21 +6,29 @@ describe("The Login Page", () => {
   const { email, password } = authUser;
   before(() => {
     cy.setCookie("Cookie_settings", "all");
-    // cy.intercept("**/api/projects", { fixture: "projects.json" });
-    // cy.intercept("**/api/screamsFrontend", { fixture: "screams.json" });
     cy.visit("/");
   });
 
-  it("replace with real test", () => {
-    cy.get("body");
-    cy.get("[data-cy=open-signnote]").click();
+  it("logs User in successfully", () => {
+    cy.get(".FilterComponent")
+      .find("[data-cy=open-signnote]")
+      .click({ force: true });
     // click on login / anmelden
-    // add data-cy to the html
     cy.get("[data-cy=login]").click();
     // fill email and password
-    cy.get("[data-cy=login-email]").fill(authUser.email);
-    cy.get("[data-cy=login-password]").fill(authUser.password);
+    cy.get("[data-cy=login-email]")
+      .click()
+      .type(authUser.email)
+    cy.get("[data-cy=login-password]")
+      .click()
+      .type(authUser.password)
     // submit button
     cy.get("[data-cy=login-user]").click();
+    // click the Profile button in Sidebar
+    cy.get(".FilterComponent")
+      .find("[data-cy=profile-button]")
+      .click({ force: true });
+    cy.get("[data-cy=hey-user]")
+      .should('contain.text', 'Hey ' + authUser.handle)
   });
 });
