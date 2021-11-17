4cbf407a553df59e74e953e7697a270424f22703
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2021-11-17 19:28:35 +0100

    add clearcache back

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ import { isMobileCustom } from "./util/customDeviceDetect";
 
 import packageJson from "../package.json";
 import { getBuildDate } from "./util/utils";
-//import withClearCache from "./ClearCache";
+import withClearCache from "./ClearCache";
 import Cookiebanner from "./components/organisms/Cookiebanner/Cookiebanner";
 import lazyLoader from "./components/atoms/Animations/LazyLoader";
 
@@ -246,5 +246,5 @@ console.log(getBuildDate(packageJson.buildDate));
 
 
 
-export default App;
-/* export default withClearCache(MainApp); */
+//export default App;
+ export default withClearCache(App)
