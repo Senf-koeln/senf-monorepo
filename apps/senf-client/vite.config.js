43e6930bfd81bd00564900cca1c910913e4f2e33
Norbertas <31192965+Digital-Coder@users.noreply.github.com>
2022-04-13 12:38:35 +0200

    disable features in vite.config.js

--- apps/senf-client/vite.config.js
+++ apps/senf-client/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
+//import { VitePWA } from "vite-plugin-pwa";
 
-import { visualizer } from "rollup-plugin-visualizer";
-import viteCompression from "vite-plugin-compression";
+//import { visualizer } from "rollup-plugin-visualizer";
+//import viteCompression from "vite-plugin-compression";
 
 // npm run build will create a file stats.html in root directory
 const pwaOptions = {
